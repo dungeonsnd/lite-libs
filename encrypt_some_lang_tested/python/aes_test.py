95bc34c634421366d71efd5fb465a3d223826d06
jeffery <dungeonsnd@gmail.com>
2015-03-09 13:46:38 +0800

    add encrypt_some_lang_tested and protector

---
+++ encrypt_some_lang_tested/python/aes_test.py
@@ -0,0 +1,24 @@
+
+
+import base64
+from Crypto.Cipher import AES
+
+import base64
+def Base64Encode(input):
+    return base64.b64encode(input)
+def Base64Decode(input):
+    return base64.b64decode(input)
+    
+import binascii
+def StrToHex(input):
+    return binascii.unhexlify(input)
+def HexToStr(input):
+    return binascii.hexlify(input)
+    
+input ='abcd'*4
+key ='a'*16
+iv ='x'*16
+cipher =AES.new(key, AES.MODE_CBC, iv).encrypt(input)
+print Base64Encode(cipher)
+print AES.new(key, AES.MODE_CBC, iv).decrypt(cipher)
+
